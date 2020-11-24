45e32ae1af84732c513ff4fe3a4cecb1f9cf43a2
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-24 22:06:13 +0100

    created a Hello World of Java Spring

--- src/main/java/hu/unideb/fupn26/App.java
+++ src/main/java/hu/unideb/fupn26/App.java
@@ -1,9 +1,37 @@
 package hu.unideb.fupn26;
 
-public class App
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Run -> Configuration -> Environment Variables
+ *  - DB_HOST localhost /127.0.0.1
+ *  - DB_PORT 3306
+ *  - DB_NAME AustralianFootball
+ *  - DB_USER root
+ *  - DB_PASS secret
+ *
+ *  Example configuration
+ * @see {project.basedir}/src/main/resources/sql/football.sh
+ */
+@Slf4j
+@SpringBootApplication
+public class App implements CommandLineRunner
 {
+    @Autowired
+    ApplicationContext context;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
     }
 }
