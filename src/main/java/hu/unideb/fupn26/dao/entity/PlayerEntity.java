84c779c1aa139dc8d09a30642ccbf2b66749145b
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-28 14:05:10 +0100

    added members and annotations to PlayerEntity.java

--- src/main/java/hu/unideb/fupn26/dao/entity/PlayerEntity.java
+++ src/main/java/hu/unideb/fupn26/dao/entity/PlayerEntity.java
@@ -1,4 +1,34 @@
 package hu.unideb.fupn26.dao.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="players", schema="AustralianFootball")
 public class PlayerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pid")
+    private int id;
+
+    @Column(name = "dob")
+    private Timestamp birthDate;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private int height;
+
+    private int weight;
 }
