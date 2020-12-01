b02670cbe3db60c2fdd80488b3699bdd1e84cab3
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-01 21:58:25 +0100

    modified function declaration in MatchStatDao.java

--- src/main/java/hu/unideb/fupn26/dao/MatchStatDao.java
+++ src/main/java/hu/unideb/fupn26/dao/MatchStatDao.java
@@ -1,12 +1,13 @@
 package hu.unideb.fupn26.dao;
 
+import hu.unideb.fupn26.exception.UnknownMatchException;
 import hu.unideb.fupn26.model.MatchStat;
 
 import java.util.Collection;
 
 public interface MatchStatDao {
 
-    void createMatchStat(MatchStat address);
+    void createMatchStat(MatchStat matchStat) throws UnknownMatchException;
     Collection<MatchStat> readAll();
     void deleteMatchStat(MatchStat matchStat);
 }
