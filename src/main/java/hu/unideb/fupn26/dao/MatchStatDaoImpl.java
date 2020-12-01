afd28db001ee1c367c037f50e7e20da439e29801
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-02 00:26:31 +0100

    implemented readAll function in MatchStatDaoImpl.java

--- src/main/java/hu/unideb/fupn26/dao/MatchStatDaoImpl.java
+++ src/main/java/hu/unideb/fupn26/dao/MatchStatDaoImpl.java
@@ -13,7 +13,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.GeneratedValue;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
@@ -68,6 +71,7 @@ public class MatchStatDaoImpl implements MatchStatDao{
             matchStatRepository.save(matchStatEntity);
         } catch (Exception e) {
             log.error("Can't save new match stat: {}", e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -149,7 +153,49 @@ public class MatchStatDaoImpl implements MatchStatDao{
 
     @Override
     public Collection<MatchStat> readAll() {
-        return null;
+        return StreamSupport.stream(matchStatRepository.findAll().spliterator(), false)
+                .map(entity -> MatchStat.builder()
+                        .playerFirstName(entity.getId().getPlayer().getFirstName())
+                        .playerLastName(entity.getId().getPlayer().getLastName())
+                        .playerTeam(entity.getTeam().getName())
+                        .opponentTeam(queryOpponentTeamName(entity))
+                        .season(entity.getId().getMatch().getSeason())
+                        .round(entity.getId().getMatch().getRound())
+                        .location(entity.getLocation())
+                        .kicks(entity.getKicks())
+                        .marks(entity.getMarks())
+                        .handballs(entity.getHandballs())
+                        .disposals(entity.getDisposals())
+                        .goals(entity.getGoals())
+                        .behinds(entity.getBehinds())
+                        .hitOuts(entity.getHitOuts())
+                        .tackles(entity.getTackles())
+                        .rebound50s(entity.getRebound50s())
+                        .inside50s(entity.getInside50s())
+                        .clearances(entity.getClearances())
+                        .clangers(entity.getClangers())
+                        .freeKicksFor(entity.getFreeKicksFor())
+                        .freeKicksAgainst(entity.getFreeKicksAgainst())
+                        .brownlowVotes(entity.getBrownlowVotes())
+                        .contestedPossessions(entity.getContestedPossessions())
+                        .uncontestedPossessions(entity.getUncontestedPossessions())
+                        .contestedMarks(entity.getContestedMarks())
+                        .marksInside50(entity.getMarksInside50())
+                        .onePercenters(entity.getOnePercenters())
+                        .bounces(entity.getBounces())
+                        .goalAssist(entity.getGoalAssist())
+                        .percentageOfGamePlayed(entity.getPercentageOfGamePlayed())
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
+
+    private String queryOpponentTeamName(MatchStatEntity entity) {
+        String playerTeamName = entity.getTeam().getName();
+        String team1Name = entity.getId().getMatch().getTeam1().getName();
+        String team2Name = entity.getId().getMatch().getTeam2().getName();
+
+        return playerTeamName.equals(team1Name) ? team2Name : team1Name;
     }
 
     @Override
