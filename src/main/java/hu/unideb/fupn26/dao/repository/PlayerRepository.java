f75486cfa5198c166b5cac1b69a2b326f5abc55f
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-01 22:23:02 +0100

    added new query method to PlayerRepository.java

--- src/main/java/hu/unideb/fupn26/dao/repository/PlayerRepository.java
+++ src/main/java/hu/unideb/fupn26/dao/repository/PlayerRepository.java
@@ -3,5 +3,9 @@ package hu.unideb.fupn26.dao.repository;
 import hu.unideb.fupn26.dao.entity.PlayerEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface PlayerRepository extends CrudRepository<PlayerEntity, Integer> {
+
+    Collection<PlayerEntity> findByFirstNameAndLastName(String firstName, String lastName);
 }
