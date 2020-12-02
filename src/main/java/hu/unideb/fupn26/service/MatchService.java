3c79d2eed2764d8d1cf8ca50548bd26c56ee1edf
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-03 00:50:50 +0100

    implemented Match service

--- src/main/java/hu/unideb/fupn26/service/MatchService.java
+++ src/main/java/hu/unideb/fupn26/service/MatchService.java
@@ -1,4 +1,17 @@
 package hu.unideb.fupn26.service;
 
+import hu.unideb.fupn26.exception.UnknownMatchException;
+import hu.unideb.fupn26.exception.UnknownTeamException;
+import hu.unideb.fupn26.model.Match;
+import hu.unideb.fupn26.model.Team;
+
+import java.util.Collection;
+
 public interface MatchService {
+
+    Collection<Match> getAllMatch() throws UnknownTeamException;
+    Collection<Match> getAllMatchByTeam(Team team);
+
+    void recordMatch(Match match) throws UnknownTeamException;
+    void deleteMatch(Match match) throws UnknownTeamException, UnknownMatchException;
 }
---
+++ src/main/java/hu/unideb/fupn26/service/MatchServiceImpl.java
@@ -0,0 +1,46 @@
+package hu.unideb.fupn26.service;
+
+import hu.unideb.fupn26.dao.MatchDao;
+import hu.unideb.fupn26.exception.UnknownMatchException;
+import hu.unideb.fupn26.exception.UnknownTeamException;
+import hu.unideb.fupn26.model.Match;
+import hu.unideb.fupn26.model.Team;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MatchServiceImpl implements MatchService{
+
+    private final MatchDao matchDao;
+
+    @Override
+    public Collection<Match> getAllMatch() {
+        return matchDao.readAll();
+    }
+
+    @Override
+    public Collection<Match> getAllMatchByTeam(Team team) {
+        return matchDao.readAll().stream()
+                .filter(match -> match.getTeam1().equals(team.getTeamName()) ||
+                        match.getTeam2().equals(team.getTeamName())
+                )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void recordMatch(Match match) throws UnknownTeamException {
+        matchDao.createMatch(match);
+    }
+
+    @Override
+    public void deleteMatch(Match match) throws UnknownTeamException, UnknownMatchException {
+        matchDao.deleteMatch(match);
+
+    }
+}
