66aa7581a2b276b105f2137a8d55e610f8ceff1a
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-02 16:20:40 +0100

    added TeamDaoImpl.java class

---
+++ src/main/java/hu/unideb/fupn26/dao/TeamDaoImpl.java
@@ -0,0 +1,23 @@
+package hu.unideb.fupn26.dao;
+
+import hu.unideb.fupn26.model.Team;
+
+import java.util.Collection;
+
+public class TeamDaoImpl implements TeamDao{
+
+    @Override
+    public void createTeam(Team team) {
+
+    }
+
+    @Override
+    public Collection<Team> readAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteMatchStat(Team team) {
+
+    }
+}
