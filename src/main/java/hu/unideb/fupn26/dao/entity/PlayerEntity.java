827246ea12d0043e921fbaaef8ebfcad3afb5f7d
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-02 22:01:38 +0100

    changed generation strategy in PlayerEntity.java

--- src/main/java/hu/unideb/fupn26/dao/entity/PlayerEntity.java
+++ src/main/java/hu/unideb/fupn26/dao/entity/PlayerEntity.java
@@ -15,7 +15,7 @@ import java.sql.Timestamp;
 public class PlayerEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "pid")
     private int id;
 
