7fc1fe1adb2b4bf1948e9932bd7e2e45ba82a356
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-03 01:39:54 +0100

    added Dto class for Team model

---
+++ src/main/java/hu/unideb/fupn26/controller/dto/TeamDto.java
@@ -0,0 +1,15 @@
+package hu.unideb.fupn26.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamDto {
+
+    private String teamName;
+}
