949041a22b3d08cfaafc73ac6b1362923b32cecf
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-05 10:05:53 +0100

    deleted CommandLineRunner inetrface implementation

--- src/main/java/hu/unideb/fupn26/App.java
+++ src/main/java/hu/unideb/fupn26/App.java
@@ -1,7 +1,13 @@
 package hu.unideb.fupn26;
 
 import hu.unideb.fupn26.dao.MatchDao;
+import hu.unideb.fupn26.dao.MatchStatDao;
+import hu.unideb.fupn26.dao.PlayerDao;
+import hu.unideb.fupn26.dao.TeamDao;
 import hu.unideb.fupn26.model.Match;
+import hu.unideb.fupn26.model.MatchStat;
+import hu.unideb.fupn26.model.Player;
+import hu.unideb.fupn26.model.Team;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,42 +29,75 @@ import org.springframework.context.ApplicationContext;
  */
 @Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
-public class App implements CommandLineRunner
+//@RequiredArgsConstructor
+public class App /*implements CommandLineRunner*/
 {
-    private final MatchDao matchDao;
+//    private final MatchDao matchDao;
+//    private final MatchStatDao matchStatDao;
+//    private final TeamDao teamDao;
+//    private final PlayerDao playerDao;
 
-    @Autowired
-    ApplicationContext context;
+//    @Autowired
+//    ApplicationContext context;
 
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello World!");
-
-        Match match = Match.builder()
-                .season(2020)
-                .round("EF")
-                .team1("Adelaide")
-                .team2("Carlton")
-                .winnerTeam("Carlton")
-                .loserTeam("Adelaide")
-                .homeTeam("Adelaide")
-                .awayTeam("Carlton")
-                .build();
-
-        try {
-            matchDao.createMatch(match);
-            matchDao.deleteMatch(match);
-            matchDao.readAll().stream()
-                    .forEach(System.out::println);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            e.printStackTrace();
-        }
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("Hello World!");
+//
+//        Match match = new Match(
+//                2020,
+//                "EF",
+//                "Adelaide",
+//                "Carlton",
+//                "h",
+//                "a",
+//                "Carlton",
+//                120,
+//                "Adelaide",
+//                34,
+//                "Adelaide",
+//                "Carlton"
+//        );
+//
+//        MatchStat matchStat = new MatchStat(
+//                "Aaron",
+//                "Black",
+//                "Carlton",
+//                "Adelaide",
+//                2020,
+//                "EF",
+//                "a"
+//        );
+//
+//        Team team = new Team("HelloTeam");
+//
+//        Player player = new Player(2020, 10, 9, "Peter", "Furjes", 190, 90);
+//        try {
+//            playerDao.createPlayer(player);
+//            playerDao.deletePlayer(player);
+//            playerDao.readAll().stream()
+//                    .forEach(System.out::println);
+//            teamDao.createTeam(team);
+//            teamDao.deleteTeam(team);
+//            teamDao.readAll().stream()
+//                    .forEach(System.out::println);
+//            matchDao.createMatch(match);
+//            matchStatDao.createMatchStat(matchStat);
+//            matchStatDao.deleteMatchStat(matchStat);
+//            matchDao.deleteMatch(match);
+//            matchDao.readAll().stream()
+//                    .forEach(System.out::println);
+////            matchStatDao.readAll().stream()
+////                    .filter(model -> model.getSeason() == 2020)
+////                    .forEach(System.out::println);
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
