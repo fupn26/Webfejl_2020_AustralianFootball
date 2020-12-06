c0a5b2b0c96fba1d2e7158b43deaf8f09eecd811
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-06 10:53:28 +0100

    added required args constructor to Team model

--- src/main/java/hu/unideb/fupn26/model/Team.java
+++ src/main/java/hu/unideb/fupn26/model/Team.java
@@ -3,11 +3,13 @@ package hu.unideb.fupn26.model;
 import lombok.*;
 
 @AllArgsConstructor
+@RequiredArgsConstructor
 @ToString
 @Getter
 @EqualsAndHashCode
 public class Team {
 
     Integer id;
+    @NonNull
     String teamName;
 }
