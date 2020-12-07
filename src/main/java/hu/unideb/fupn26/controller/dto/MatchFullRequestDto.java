185893c4f68cd389f8674d268ac563edac186496
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 08:58:20 +0100

    created new Match DTO-s based on the new model

--- src/main/java/hu/unideb/fupn26/controller/dto/MatchFullRequestDto.java
+++ src/main/java/hu/unideb/fupn26/controller/dto/MatchFullRequestDto.java
@@ -3,39 +3,20 @@ package hu.unideb.fupn26.controller.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MatchDto {
+public class MatchFullRequestDto extends MatchMinimalRequestDto{
 
-    private Integer season;
-    private String round;
-    private String team1;
-    private String team2;
-    private String team1Location;
-    private String team2Location;
-    private int yearOfStart;
-    private int monthOfStart;
-    private int dayOfStart;
-    private int hourOfStart;
-    private int minuteOfStart;
-    private int secondOfStart;
+    private String startDate;
     private String venue;
     private Integer attendants;
     private Integer margin;
-    private String winnerTeam;
-    private Integer winnerScore;
-    private String winnerLocation;
-    private String loserTeam;
-    private Integer loserScore;
-    private String loserLocation;
-    private Integer homeScore;
     private Integer homeQ1Goals;
     private Integer homeQ2Goals;
     private Integer homeQ3Goals;
@@ -46,7 +27,6 @@ public class MatchDto {
     private Integer homeQ3Behinds;
     private Integer homeQ4Behinds;
     private Integer homeExtraTimeBehinds;
-    private Integer awayScore;
     private Integer awayQ1Goals;
     private Integer awayQ2Goals;
     private Integer awayQ3Goals;
--- src/main/java/hu/unideb/fupn26/controller/dto/MatchResponseDto.java
+++ src/main/java/hu/unideb/fupn26/controller/dto/MatchResponseDto.java
@@ -3,14 +3,16 @@ package hu.unideb.fupn26.controller.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
+
+import java.sql.Timestamp;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MatchRequestDto extends MatchDto{
+public class MatchResponseDto extends MatchFullRequestDto {
 
-    private String homeTeam;
-    private String awayTeam;
+    private Integer id;
 }
