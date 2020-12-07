1fbfd02fb6097d42150c565053984f8d1109221e
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 01:17:05 +0100

    added InvalidMatchArgumentException.java

---
+++ src/main/java/hu/unideb/fupn26/exception/InvalidMatchArgumentException.java
@@ -0,0 +1,8 @@
+package hu.unideb.fupn26.exception;
+
+public class InvalidMatchArgumentException extends Exception {
+
+    public InvalidMatchArgumentException(String message) {
+        super(message);
+    }
+}
