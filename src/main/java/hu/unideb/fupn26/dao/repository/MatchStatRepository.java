7c989ea5a93b900cc37815fac9fad85e7bee46c5
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 10:56:15 +0100

    added findById_Match_Id function to MatchStatRepository.java

--- src/main/java/hu/unideb/fupn26/dao/repository/MatchStatRepository.java
+++ src/main/java/hu/unideb/fupn26/dao/repository/MatchStatRepository.java
@@ -4,5 +4,9 @@ import hu.unideb.fupn26.dao.entity.MatchStatEntity;
 import hu.unideb.fupn26.dao.entity.MatchStatId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface MatchStatRepository extends CrudRepository<MatchStatEntity, MatchStatId> {
+
+    Collection<MatchStatEntity> findById_Match_Id(String id);
 }
