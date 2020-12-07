b197eda94329b45885d2c1ab80d0cfb19aa4104a
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 10:56:32 +0100

    added MatchSqlIntegrityException.java

---
+++ src/main/java/hu/unideb/fupn26/exception/MatchSqlIntegrityException.java
@@ -0,0 +1,8 @@
+package hu.unideb.fupn26.exception;
+
+public class MatchSqlIntegrityException extends Exception {
+
+    public MatchSqlIntegrityException(String message) {
+        super(message);
+    }
+}
