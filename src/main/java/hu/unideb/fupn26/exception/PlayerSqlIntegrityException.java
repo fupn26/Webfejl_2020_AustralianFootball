1667c1987df41db974a46ea3cdd7f9f0f751ee48
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 19:10:53 +0100

    added PlayerSqlIntegrityException

---
+++ src/main/java/hu/unideb/fupn26/exception/PlayerSqlIntegrityException.java
@@ -0,0 +1,8 @@
+package hu.unideb.fupn26.exception;
+
+public class PlayerSqlIntegrityException extends Exception {
+
+    public PlayerSqlIntegrityException(String message) {
+        super(message);
+    }
+}
