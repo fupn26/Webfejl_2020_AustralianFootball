ec76f202801824042b1f0729c24abd2862405c02
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-07 17:07:30 +0100

    added new query functions

--- src/main/java/hu/unideb/fupn26/dao/repository/MatchRepository.java
+++ src/main/java/hu/unideb/fupn26/dao/repository/MatchRepository.java
@@ -1,7 +1,12 @@
 package hu.unideb.fupn26.dao.repository;
 
 import hu.unideb.fupn26.dao.entity.MatchEntity;
+import hu.unideb.fupn26.dao.entity.TeamEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface MatchRepository extends CrudRepository<MatchEntity, String> {
+
+    Collection<MatchEntity> findByTeam1OrTeam2(TeamEntity entity1, TeamEntity entity2);
 }
--- src/main/java/hu/unideb/fupn26/dao/repository/MatchStatRepository.java
+++ src/main/java/hu/unideb/fupn26/dao/repository/MatchStatRepository.java
@@ -2,6 +2,7 @@ package hu.unideb.fupn26.dao.repository;
 
 import hu.unideb.fupn26.dao.entity.MatchStatEntity;
 import hu.unideb.fupn26.dao.entity.MatchStatId;
+import hu.unideb.fupn26.dao.entity.TeamEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Collection;
@@ -9,4 +10,5 @@ import java.util.Collection;
 public interface MatchStatRepository extends CrudRepository<MatchStatEntity, MatchStatId> {
 
     Collection<MatchStatEntity> findById_Match_Id(String id);
+    Collection<MatchStatEntity> findByTeam(TeamEntity entity);
 }
