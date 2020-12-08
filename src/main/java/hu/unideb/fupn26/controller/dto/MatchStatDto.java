e5654f999a274fabad3c95dd2c50e638e94eda4d
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-08 14:54:04 +0100

    added MatchStatDto

---
+++ src/main/java/hu/unideb/fupn26/controller/dto/MatchStatDto.java
@@ -0,0 +1,33 @@
+package hu.unideb.fupn26.controller.dto;
+
+public class MatchStatDto {
+
+    private String matchId;
+    private Integer playerId;
+    private Integer teamId;
+    private Integer goals;
+    private Integer behinds;
+
+
+    private Integer kicks;
+    private Integer marks;
+    private Integer handballs;
+    private Integer disposals;
+    private Integer hitOuts;
+    private Integer tackles;
+    private Integer rebound50s;
+    private Integer inside50s;
+    private Integer clearances;
+    private Integer clangers;
+    private Integer freeKicksFor;
+    private Integer freeKicksAgainst;
+    private Integer brownlowVotes;
+    private Integer contestedPossessions;
+    private Integer uncontestedPossessions;
+    private Integer contestedMarks;
+    private Integer marksInside50;
+    private Float onePercenters;
+    private Integer bounces;
+    private Integer goalAssist;
+    private Float percentageOfGamePlayed;
+}
