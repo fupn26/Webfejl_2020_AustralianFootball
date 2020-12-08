0c55857cb05a05fe3ff51d87b9a2815673427ac0
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-08 10:59:31 +0100

    added InvalidMatchStatArgumentException.java

---
+++ src/main/java/hu/unideb/fupn26/exception/InvalidMatchStatArgumentException.java
@@ -0,0 +1,8 @@
+package hu.unideb.fupn26.exception;
+
+public class InvalidMatchStatArgumentException extends Exception{
+
+    public InvalidMatchStatArgumentException(String message) {
+        super(message);
+    }
+}
