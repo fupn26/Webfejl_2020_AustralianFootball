23ab9aa118dd77a65684cc5fb3b76f5ca05ecf7c
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-09 11:11:38 +0100

    added MatchStatDaoImplTest.java

---
+++ src/test/java/hu/unideb/fupn26/dao/MatchStatDaoImplTest.java
@@ -0,0 +1,24 @@
+package hu.unideb.fupn26.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchStatDaoImplTest {
+
+    @Test
+    void createMatchStat() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void updateMatchStat() {
+    }
+
+    @Test
+    void deleteMatchStat() {
+    }
+}
